db836ab859449b1b0710a699d288bb30958114b8
Ujjwal pandey <pandey.ujjwal7488@gmail.com>
2026-09-14 00:57:26 +0530

    Time: 14 ms (7.05%), Space: 43 MB (84.53%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,22 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        HashMap<Character, Integer> map=  new HashMap<> ();
+        for(int i=0; i<s.length(); i++){
+            char ch=s.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch) +1);
+            }else{
+                map.put(ch, 1);
+            }
+        }
+        for(int i=0; i<t.length(); i++){
+            char ch=t.charAt(i);
+            if(!map.containsKey(ch) || map.get(ch)==0){
+                return ch;
+            }
+            map.put(ch, map.get(ch)-1);     
+        }
+        return ' ';          
+    }
+}
