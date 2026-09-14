8df6ac4739190324b993aeeb3136b132d876ae56
Ujjwal pandey <pandey.ujjwal7488@gmail.com>
2026-09-15 00:48:06 +0530

    Time: 4 ms (15.46%), Space: 43.8 MB (75.26%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int d1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int d2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+
+            int sum = d1 + d2 + carry;
+            carry = sum / 10;
+            res.append(sum % 10);
+
+            i--;
+            j--;
+        }
+
+        return res.reverse().toString();
+    }
+}
